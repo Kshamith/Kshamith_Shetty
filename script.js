f6ac677f7ce57ee6af8ffaf9fe6000b1b2b30052
Kshamith Shetty <Kshamithrajshetty@gmail.com>
2024-02-28 20:00:57 +0530

    Updates

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ var lightColors = {
     '--background': '#f5f5f5',
     '--primary': '#181a1a',
     '--muted': '#cbc7c7',
-    '--accent': '#ff4805',
+    '--accent': '#ff5112',
     '--font': '#181a1a'
 };
 
 var darkColors = {
     '--background': '#0F1C2E',
     '--primary': '#0055c4',
-    '--muted': '',
-    '--accent': '#ff4805',
+    '--muted': '#1F3A5F',
+    '--accent': '#ff5112',
     '--font': '#f5f5f5f5'
 };
 
@@ -33,23 +33,20 @@ toggleButton.addEventListener('click', function() {
     isLightMode = !isLightMode;
 });
 
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
 
 
-
-
-// Get the button and icon elements
 var button = document.getElementById('color-toggle');
 var icon = document.getElementById('myIcon');
 
-// Add event listener for the button click
 button.addEventListener('click', function() {
-    // Check the current icon class
     if (icon.classList.contains('ri-moon-fill')) {
-        // If it's the moon icon, change it to the sun icon
         icon.classList.remove('ri-moon-fill');
         icon.classList.add('ri-sun-fill');
     } else {
-        // If it's the sun icon, change it back to the moon icon
         icon.classList.remove('ri-sun-fill');
         icon.classList.add('ri-moon-fill');
     }
